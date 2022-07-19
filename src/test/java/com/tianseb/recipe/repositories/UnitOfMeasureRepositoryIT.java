15523ef787bfb5c7530fdbf2cd1783423b9d3ce0
TianSeb <sebastianalasia@hotmail.com>
2022-07-18 21:13:24 -0300

    Circle CI test

--- src/test/java/com/tianseb/recipe/repositories/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/tianseb/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -3,15 +3,10 @@ package com.tianseb.recipe.repositories;
 import com.tianseb.recipe.domain.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import java.lang.management.OperatingSystemMXBean;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
